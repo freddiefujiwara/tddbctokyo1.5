cb4efcb1f491781303ff7baf46658d44d67b0ad5
Freddie FUJIWARA <github@zeg.la>
2011-07-09 11:49:01 +0900

    message_filter move to directory

---
+++ message_filter/message_filter.rb
@@ -0,0 +1,9 @@
+class MessageFilter
+	def initialize(word)
+	  @word = word
+	end
+	def detect?(text)
+	  text.include?(@word)
+	end
+
+end
---
+++ message_filter/message_filter_spec.rb
@@ -0,0 +1,14 @@
+require 'rubygems'
+require 'rspec'
+require 'message_filter'
+
+describe MessageFilter do
+	it 'should detect message with NG word' do
+		filter = MessageFilter.new('foo')
+		filter.detect?('hello from foo').should == true
+	end
+	it 'should not detect message with NG word' do
+		filter = MessageFilter.new('foo')
+		filter.detect?('hello, world!').should == false
+	end
+end
